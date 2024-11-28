7b1b2641f37344249d3c11493230cf0c046d29a8
benjamin <77669198+aname4me-String@users.noreply.github.com>
2024-11-28 11:01:44 +0100

    validation update now not crashing on invalid creds
    also fixed updating

--- routing/validation.js
+++ routing/validation.js
@@ -9,69 +9,84 @@ const {userDN,newUser, updateUser} = require('../db/queries');
 async function login(req, res, next) {
     let { username, password } = req.body;
     if(!username || !password) return res.status(400).send('Login daten fehlen');
+
     let daten;
-    let client;
+
     try {
-        client = ldap.createClient({
+        let client = ldap.createClient({
             url: env.LDAP_URL,
         });
 
-        let searchOptions = {
-            filter: `(&(mailNickname=${username})(objectClass=user))`,
-            scope: "sub",
-            attributes: ["mailNickname", "department", "employeeType", "sn", "givenName"],
-        };
+        daten = await new Promise((resolve, reject) => {
+            client.bind("tgm\\"+username, password, (err,res) => {
+                if(err)
+                    reject(err);
+                resolve(res);
+            });
+        }).then(async () => {
+            let searchOptions = {
+                filter: `(&(mailNickname=${username})(objectClass=user))`,
+                scope: "sub",
+                attributes: ["mailNickname", "department", "employeeType", "sn", "givenName"],
+            };
 
-        await client.bind("tgm\\"+username, password, (err) => {
-            if(err)console.log(err)});
+            return await ldapSuche(client, searchOptions);
+        });
 
-        daten = await ldapSuche(client, searchOptions);
-        console.log('Search results:', daten);
     } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        client.unbind();
+        return res.status(401).json({
+            success: -1,
+            error: error.name,
+        });
     }
 
     if(!daten) return res.status(401).send('Login ungültig');
 
-    let user = (await userDN(username))[0];
+    let user = (await userDN(daten.mailNickname))[0];
     console.log(user);
     let jg = daten.department.slice(0,1);
     let kl = daten.department.slice(1,2);
     let ab = daten.department.slice(2);
 
-    if (user.length === 0) {
+    if (!user) {
         let dn = daten.mailNickname;
         let vn = daten.givenName.split(" ")[0];
         let sn = daten.sn;
         let type = daten.employeeType;
         newUser(dn, dn, vn, sn, jg, kl, ab, type);
-        user = (await userDN(username))[0];
     } else {
         updateUser(user.uuid, jg, kl, ab);
     }
+    user = (await userDN(daten.mailNickname))[0];
 
     const token = jwt.sign(
-        { username: user.distinguishedName, role: user.type }, jwt_key,
-        { expiresIn: '4h' }
+        { vn: user.vn, username: user.distinguishedName, role: user.type }, jwt_key,
+        { expiresIn: '8h' }
     );
 
-    return res.status(200).send("Login Successful: "+token);
+    return res.status(200).json({
+        success: 0,
+        error: "",
+        data: {
+            token: token,
+        }
+    });
 }
 
 async function ldapSuche(client, searchOptions) {
     return new Promise((resolve, reject) => {
-        let daten;
         client.search(env.LDAP_BASE_DN, searchOptions, (err, res) => {
-            if (err) {return reject(err);}
 
             res.on('searchEntry', (entry) => {
-                daten = entry.object;
+                resolve(entry.object);
             });
 
+            res.on('error', (err) => {
+                reject(err);
+            })
+
             res.on('end', () => {
-                resolve(daten);
+                client.unbind();
             });
         });
 
@@ -88,7 +103,7 @@ function authenticateRole (roles) {
             if (err) return res.status(401).send(err.message);
 
             if (!roles.includes(user.role))
-                return res.status(401).send('Unzureichende Berechtigungen.');
+                return res.status(403).send('Unzureichende Berechtigungen.');
 
             req.role = user;
             next();
