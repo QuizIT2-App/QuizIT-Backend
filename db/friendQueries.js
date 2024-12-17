f8a05068c5096205a024b04973564d90f35b0a52
benjamin <77669198+aname4me-String@users.noreply.github.com>
2024-12-17 13:47:18 +0100

    added and updated friend funktions with check for pending and requested

--- db/friendQueries.js
+++ db/friendQueries.js
@@ -9,11 +9,19 @@ async function getFriends(id) {
         });
     });
 }
-
 async function getPending(id) {
     return new Promise(async (resolve, reject) => {
-        db.query('SELECT user.* FROM (SELECT DISTINCT CASE WHEN id1 = ? THEN id2 ELSE id1 END AS id FROM friends f1 WHERE (id1 = ? OR id2 = ?) AND NOT EXISTS (SELECT 1 FROM friends f2 WHERE f1.id1 = f2.id2 AND f1.id2 = f2.id1)) AS friend JOIN User user ON friend.id = user.uuid;',
-            [id, id, id], (err, result) => {
+        db.query('SELECT user.* FROM (SELECT DISTINCT id1 AS id FROM friends f1 WHERE id2 = ? AND NOT EXISTS (SELECT 1 FROM friends f2 WHERE f1.id1 = f2.id2 AND f1.id2 = f2.id1)) AS friend JOIN User user ON friend.id = user.uuid;',
+            [id], (err, result) => {
+                if (err) reject(err);
+                resolve(result);
+            });
+    })
+}
+async function getRequested(id) {
+    return new Promise(async (resolve, reject) => {
+        db.query('SELECT user.* FROM (SELECT DISTINCT id2 AS id FROM friends f1 WHERE id1 = ? AND NOT EXISTS (SELECT 1 FROM friends f2 WHERE f1.id1 = f2.id2 AND f1.id2 = f2.id1)) AS friend JOIN User user ON friend.id = user.uuid;',
+            [id], (err, result) => {
                 if (err) reject(err);
                 resolve(result);
             });
@@ -21,10 +29,14 @@ async function getPending(id) {
 }
 
 function addFriend(id1,id2) {
-    db.query("INSERT INTO friends(id1, id2) VALUES(?, ?)", [id1, id2]);
+    db.query('INSERT INTO friends(id1, id2) VALUES(?, ?)', [id1, id2], (err, result) => {
+        if(err)
+            console.log(err);
+    });
 }
 
 function deleteFriend(id1, id2) {
+    db.query('DELETE FROM friends WHERE (id1 = ? AND id2 = ?) OR (id1 = ? AND id2 = ?) ', [id1, id2, id2, id1]);
 }
 
 
@@ -32,6 +44,7 @@ function deleteFriend(id1, id2) {
 module.exports = {
     getFriends,
     getPending,
+    getRequested,
     addFriend,
     deleteFriend
 };
--- endpoints/friendEndpoints.js
+++ endpoints/friendEndpoints.js
@@ -1,26 +1,41 @@
 const {returnHTML} = require("../utils/utils");
-const {getFriends, getPending} = require("../db/friendQueries");
+const {getFriends, getPending, getRequested, deleteFriend, addFriend} = require("../db/friendQueries");
 
 async function getSelfFriends(req, res) {
-    let items = await getFriends(req.token.id);
+    let items = await getFriends(req.user.id);
     return returnHTML(res,200,{data: items});
 }
 
 async function getSelfPending(req, res) {
-    let items = await getPending(req.token.id);
+    let items = await getPending(req.user.id);
     return returnHTML(res,200,{data: items});
+}
 
+async function getSelfRequested(req, res) {
+    let items = await getRequested(req.user.id);
+    return returnHTML(res,200,{data: items});
 }
 
 async function addSelfFriend(req, res) {
-    body=req.body;
+    let {id} = req.body;
+    if(!id)
+        returnHTML(res, 400, {error:"BadRequestError"})
+    addFriend(req.user.id, id);
+    return returnHTML(res,200,{data: "friend request successful"});
 }
 
-async function deleteSelfFriend(req, res) {}
+async function deleteSelfFriend(req, res) {
+    let {id} = req.body;
+    if(!id)
+        returnHTML(res, 400, {error:"BadRequestError"})
+    deleteFriend(req.user.id, id);
+    return returnHTML(res,200,{data: "removed friend"});
+}
 
 module.exports = {
     getFriends: getSelfFriends,
     getPending: getSelfPending,
+    getRequested: getSelfRequested,
     addFriend: addSelfFriend,
     deleteFriend: deleteSelfFriend,
 }
