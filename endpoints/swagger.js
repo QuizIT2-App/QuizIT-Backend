e1b37f1c7b4588531962028aebf19ebcfc45e013
benjamin <77669198+aname4me-String@users.noreply.github.com>
2024-12-18 04:58:54 +0100

    new schemas for easier documentaion

--- endpoints/swagger.js
+++ endpoints/swagger.js
@@ -54,76 +54,93 @@ const options = {
             uuid: {
               type: 'integer',
               description: 'Unique identifier for the user',
+              example: 1
             },
             name: {
               type: 'string',
-              description: 'displayName of the user'
+              description: 'displayName of the user',
+              example: 'birni',
             },
             punkte: {
               type: 'integer',
-              description: 'Points the user has'
+              description: 'Points the user has',
+              example: 9999
             },
             isme: {
               type: 'boolean',
-              description: 'boolean if it is the current user'
+              description: 'boolean if it is the current user',
+              example: true
             }
           }
         },
-        User: {
-          type: object,
+        user: {
+          type: 'object',
           properties: {
             uuid: {
               type: 'integer',
+              description: 'Unique identifier for the user',
               example: 1
             },
             displayName: {
               type: 'string',
+              description: 'displayName of the user',
               example: 'maxi'
             },
             distinguishedName: {
               type: 'string',
+              description: 'distinguishedName of the user',
               example: 'mmusterman'
             },
             vorname: {
               type: 'string',
+              description: 'firstname of the user',
               example: 'Max'
             },
             nachname: {
               type: 'string',
+              description: 'surname of the user',
               example: 'Musterman'
             },
             jahrgang: {
               type: 'string',
+              description: 'schoolyear of the user',
               example: 1
             },
             klasse: {
               type: 'string',
-              example: a
+              description: 'class of the user',
+              example: 'a'
             },
             abteilung: {
               type: 'string',
+              description: 'department of the user',
               example: 'HIT'
             },
             punkte: {
               type: 'integer',
+              description: 'Points the user has',
               example: 0
             },
             streak: {
               type: 'integer',
+              description: 'daily streak the user has',
               example: 0
             },
             erstelldatum: {
               type: 'string',
+              description: 'creation date of the user',
               format: 'date-time',
               example: '2024-12-12T13:57:01.000Z'
             },
             zuletztOn: {
               type: 'string',
+              description: 'last time online of the user',
               format: 'date-time',
               example: '2024-12-12T13:57:01.000Z'
             },
             type: {
               type: 'string',
+              description: 'type of user',
               example: 'schueler'
             }
           }
