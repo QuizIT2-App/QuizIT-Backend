c85cc9b514514b38d799176fe0c0d4b920c9e636
benjamin <77669198+aname4me-String@users.noreply.github.com>
2024-12-18 14:44:46 +0100

    allowed cors for api purposes

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const app = express();
 const PORT = env.PORT || 3000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  next();
+});
 app.use(generalRouter);
 app.use(userRouter);
 app.use(friendRouter);
