aadf7394d589c639519c18bb0c0bf1da143d3602
benjamin <77669198+aname4me-String@users.noreply.github.com>
2024-12-18 05:02:13 +0100

    added documentation through swagger

---
+++ routing/userRoutes.js
@@ -0,0 +1,142 @@
+const router = require('express').Router();
+const {authAll, authLA, authAdmin} = require('./validation');
+const {getSelf, deleteSelf} = require('../endpoints/userEndpoints');
+
+/**
+ * @swagger
+ * /self:
+ *   get:
+ *     summary: get own user
+ *     description: This endpoint endpoints allows one to get ones own user data.
+ *     tags:
+ *       - User
+ *     security:
+ *       - Authorization: [schueler, lehrer, admin]
+ *     responses:
+ *       200:
+ *         description: Successful request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                    $ref: '#/components/schemas/user'
+ *
+ *       400:
+ *         description: MissingCredentialsError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: MissingCredentialsError
+ *       401:
+ *         description: JsonWebTokenError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: JsonWebTokenError
+ *       403:
+ *         description: InsufficientPermissionsError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: InsufficientPermissionsError
+ */
+router.get('/self', authAll, getSelf);
+router.post('/self', authAll, );                //TODO
+
+/**
+ * @swagger
+ * /self:
+ *   delete:
+ *     summary: delete own user
+ *     description: This endpoint endpoints allows one to delete ones own user.
+ *     tags:
+ *       - User
+ *     security:
+ *       - Authorization: [schueler, lehrer, admin]
+ *     responses:
+ *       200:
+ *         description: User successfully Deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     text:
+ *                       type: string
+ *                       example: User Deleted
+ *       400:
+ *         description: MissingCredentialsError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: MissingCredentialsError
+ *       401:
+ *         description: JsonWebTokenError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: JsonWebTokenError
+ *       403:
+ *         description: InsufficientPermissionsError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: InsufficientPermissionsError
+ */
+router.delete('/self', authAll, deleteSelf);
+
+
+module.exports = router;
