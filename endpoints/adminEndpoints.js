50023c540e40f9f75e367b2013d072e534271aa9
benjamin <77669198+aname4me-String@users.noreply.github.com>
2024-12-19 17:10:09 +0100

    changes

--- endpoints/adminEndpoints.js
+++ endpoints/adminEndpoints.js
@@ -2,7 +2,12 @@ const {returnHTML} = require("../utils/utils");
 const {update} = require('../utils/updateRoutine');
 
 async function adminUpdate(req, res) {
-    return returnHTML(res, 200, {data: "test"})
+    try {
+        await update(req, res);
+        return returnHTML(res, 200, {data: "Update completed successfully"});
+    }catch(err) {
+        return returnHTML(res, 500, {error: "something went wrong"});
+    }
 }
 module.exports = {
     update: adminUpdate,
