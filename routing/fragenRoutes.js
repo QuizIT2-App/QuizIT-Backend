765d502b8e383e6652df3c3dd84bfd8022125390
benjamin <77669198+aname4me-String@users.noreply.github.com>
2025-01-09 15:07:56 +0100

    oops

--- routing/fragenRoutes.js
+++ routing/fragenRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const {authAll, authLA, authAdmin} = require('./validation');
 const {getQuizes} = require("../endpoints/fragenEndpoints");
 
-router.get('/quiz/:id', authLA, getQuizes);
+router.get('/question/:id', authLA, getQuizes);
 
 module.exports = router;
