18c9bbbb7992b5b1f1e679214d7cd7c4fc3b8c77
benjamin <77669198+aname4me-String@users.noreply.github.com>
2025-01-09 11:04:48 +0100

    fixed error

--- routing/quizRoutes.js
+++ routing/quizRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {authAll, authLA, authAdmin} = require('./validation');
 const {getQuizes, getSubQuizes} = require("../endpoints/quizEndpoints");
 
-router.get('quiz', authAll, getQuizes);
-router.get('quiz/:id', authAll, getSubQuizes);
+router.get('/quiz', authAll, getQuizes);
+router.get('/quiz/:id', authAll, getSubQuizes);
 
 module.exports = router;
