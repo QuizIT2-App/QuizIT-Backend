d558a354c7719e9d7e161735369eb52cbf443d03
Gordlby <77514220+Gordlby@users.noreply.github.com>
2025-03-02 19:47:58 +0100

    [Fragen] Add getCurrentQuiz Demo Function without db

--- routing/fragenRoutes.js
+++ routing/fragenRoutes.js
@@ -1,6 +1,106 @@
 const router = require("express").Router();
 const { authAll, authLA, authAdmin } = require("./validation");
 const { getQuizes, getCurrentQuiz } = require("../endpoints/fragenEndpoints");
+
+/**
+ * @swagger
+ * /question/currentquiz/:
+ *   get:
+ *     summary: get current quiz questions
+ *     description: This endpoint allows one to access all questions in a pool
+ *     tags:
+ *       - Question
+ *     security:
+ *       - Authorization: [schueler, lehrer, admin]
+ *     responses:
+ *       200:
+ *         description: Successful request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       runId:
+ *                         type: integer
+ *                         example: 200
+ *                       question:
+ *                         type: object
+ *                         properties:
+ *                           title:
+ *                             type: string
+ *                             example: "Was ist 1+1?"
+ *                           type:
+ *                             type: integer
+ *                             example: 1
+ *                           options:
+ *                             type: array
+ *                             items:
+ *                               type: object
+ *                               properties:
+ *                                 id:
+ *                                   type: integer
+ *                                 text:
+ *                                   type: string
+ *                               example:
+ *                               - id: 1
+ *                                 text: "1"
+ *                               - id: 2
+ *                                 text: "2"
+ *                               - id: 3
+ *                                 text: "3"
+ *                               - id: 4
+ *                                 text: "4"
+ * 
+ *       400:
+ *         description: MissingCredentialsError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: MissingCredentialsError
+ *       401:
+ *         description: JsonWebTokenError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: JsonWebTokenError
+ *       403:
+ *         description: InsufficientPermissionsError
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   example: InsufficientPermissionsError
+ */
+router.get("/question/currentquiz/", authAll, getCurrentQuiz);
+
 /**
  * @swagger
  * /question/{quizId}:
@@ -75,72 +175,5 @@ const { getQuizes, getCurrentQuiz } = require("../endpoints/fragenEndpoints");
  */
 router.get("/question/:id", authLA, getQuizes);
 
-
-/**
- * @swagger
- * /question/currentquiz/:
- *   get:
- *     summary: get current quiz questions
- *     description: This endpoint allows one to access all questions in a pool
- *     tags:
- *       - Question
- *     security:
- *       - Authorization: [schueler, lehrer, admin]
- *     responses:
- *       200:
- *         description: Successful request
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                 data:
- *                   type: array
- *                   items:
- *                     $ref: '#/components/schemas/question'
- *       400:
- *         description: MissingCredentialsError
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                 error:
- *                   type: string
- *                   example: MissingCredentialsError
- *       401:
- *         description: JsonWebTokenError
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                 error:
- *                   type: string
- *                   example: JsonWebTokenError
- *       403:
- *         description: InsufficientPermissionsError
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                 error:
- *                   type: string
- *                   example: InsufficientPermissionsError
- */
-router.get("/question/currentquiz/", authAll, getCurrentQuiz);
 
 module.exports = router;
