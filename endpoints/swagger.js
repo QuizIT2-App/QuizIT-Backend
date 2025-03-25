71ad377b6b1d26bfa447f929ce5eaf2e27756831
Gordlby <77514220+Gordlby@users.noreply.github.com>
2025-03-25 23:44:17 +0100

    [Swagger] Add selfhosted domain to servers-list

--- endpoints/swagger.js
+++ endpoints/swagger.js
@@ -5,7 +5,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'QuizIT API',
+      title: 'QuizIT APIx',
       version: '1.0.0',
       description: 'API documentation for the QuizIT project',
     },
