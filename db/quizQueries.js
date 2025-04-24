f06552b47f27f91819eb3a45b619f1945a6f6de7
benjamin <77669198+aname4me-String@users.noreply.github.com>
2025-04-24 15:28:04 +0200

    dings

--- db/quizQueries.js
+++ db/quizQueries.js
@@ -117,7 +117,7 @@ async function closeOpenQuizzes(user, callback) {
                         if (lastIdResulterror) {
                             errorLog(lastIdResulterror);
                         }
-                        resultid = lastIdResult;
+                        resultid = lastIdResult.asd;
                         connection.commit();
                         connection.release();
                     })
